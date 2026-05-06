1ca351b32112cb7fc4f9b4e5bb530cf694957918
piggecutlet <234113501+piggecutlet@users.noreply.github.com>
2026-05-06 13:00:03 +0900

    pnpm fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import perfectionist from "eslint-plugin-perfectionist";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
