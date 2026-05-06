88864250cc3ece62064a447bbde8934d8527111d
piggecutlet <234113501+piggecutlet@users.noreply.github.com>
2026-05-06 10:19:07 +0900

    eslint.config.mjs を調整

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,30 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import perfectionist from "eslint-plugin-perfectionist";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
+    // 以下は ESLint がデフォルトで除外
+    // ソース: node_modules\.pnpm\eslint@9.39.4\node_modules\eslint\lib\config\default-config.js#L66
+    // ドキュメント: https://eslint.org/docs/latest/use/configure/configuration-files#globally-ignore-files-with-ignores
+    "node_modules/**",
+
+    // 以下は eslint-config-next がデフォルトで除外
+    // ソース: node_modules/eslint-config-next/dist/index.js#L209
+    // ドキュメント: https://nextjs.org/docs/app/api-reference/config/eslint
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
+
+  // 自然順でソート
+  // ドキュメント: https://perfectionist.dev/configs/recommended-natural
+  perfectionist.configs["recommended-natural"],
 ]);
 
 export default eslintConfig;
